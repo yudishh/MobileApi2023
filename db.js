ea5ae2906cdbbbe7ccc2f59f4e1a98a9afea6cd3
yudishh <121374183+yudishh@users.noreply.github.com>
2023-02-20 20:53:28 +0400

    Add files via upload

---
+++ db.js
@@ -0,0 +1,15 @@
+const DressCode = require("./data/DressCode");
+const EventCategory = require("./data/EventCategory");
+const eventStatus = require("./data/eventStatus");
+const EventList = require("./data/EventList");
+const Venue = require("./data/Venue");
+const Users = require("./data/Users");
+
+module.exports = {
+    DressCode,
+    EventCategory,
+	eventStatus,
+    EventList,
+	Venue,
+	Users
+}
